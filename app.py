256e4bbc1c7482225a8320034e92f23ab975c53a
Oliver Kohl <inhaber@conteco.gmbh>
2023-05-26 09:45:42 +0200

    Add links and rename file

--- app.py
+++ app.py
@@ -28,6 +28,9 @@
 # https://github.com/OliverKohlDSc/flask_demo_app
 # https://azure.microsoft.com/en-us/products/app-service/
 # https://learn.microsoft.com/en-us/azure/app-service/
+# https://learn.microsoft.com/en-us/azure/app-service/quickstart-python?tabs=flask%2Cwindows%2Cazure-cli%2Cvscode-deploy%2Cdeploy-instructions-azportal%2Cterminal-bash%2Cdeploy-instructions-zip-azcli
+
+# https://<app-name>.azurewebsites.net
 
 import flask
 
@@ -37,3 +40,7 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
